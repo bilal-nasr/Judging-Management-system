01c1fe02ac117b9adda2636255c44a5c89a1ca48
diaa zaytouni <diaazeitouni@gmail.com>
2023-08-22 17:49:33 +0300

    h

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2/promise");
 const cors = require("cors");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 const PORT = 3006;
 
 const pool = mysql.createPool({
@@ -12,28 +12,26 @@ const pool = mysql.createPool({
   user: "root",
   database: "test",
   password: "Bilal@2711",
-  port:"3306"
+  port: "3306",
 });
 
 app.use(bodyParser.json());
 
 app.get("/getdata", async (req, res) => {
-    try {
-        // Assuming you want to use a query parameter to filter data, you can access it as req.query.parameter_name
-        // Example: const name = req.query.name;
+  try {
+    // Assuming you want to use a query parameter to filter data, you can access it as req.query.parameter_name
+    // Example: const name = req.query.name;
 
-        // If you want to fetch all data from the "users" table, you can use a simple SELECT query like this:
-        const [rows] = await pool.execute("SELECT * FROM users");
+    // If you want to fetch all data from the "users" table, you can use a simple SELECT query like this:
+    const [rows] = await pool.execute("SELECT * FROM users");
 
-        // Send the fetched data as a JSON response
-        res.json(rows);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        res.status(500).json({ success: false, message: "Error fetching data" });
-      }
+    // Send the fetched data as a JSON response
+    res.json(rows);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ success: false, message: "Error fetching data" });
+  }
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
