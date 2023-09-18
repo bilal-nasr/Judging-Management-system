3c8fb3f2e44f6c0ada0ed6b2f72171615637e30b
bilal-nasr <bilal.nasr2711@gmail.com>
2023-09-18 18:34:56 +0300

    Update LoginPage.js

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -1,11 +1,20 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   TextField,
   Button,
   Card,
   CardContent,
   Typography,
+  InputAdornment,
+  IconButton,
+  Container,
+  Box,
 } from "@mui/material";
+import {
+  Visibility,
+  VisibilityOff,
+} from "@mui/icons-material";
+import PersonIcon from "@mui/icons-material/Person";
 import api from "../api";
 import Auth from "../Auth";
 import { useNavigate } from "react-router-dom";
@@ -16,41 +25,40 @@ const LoginPage = () => {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    if(Auth.isUserAuthenticated())
-        navigate("/dashboard")
-  },[navigate]);
+  const [showPassword, setShowPassword] = React.useState(false);
 
-  // todo here
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  useEffect(() => {
+    if (Auth.isUserAuthenticated()) navigate("/dashboard");
+  }, [navigate]);
+
   const handleLogin = async () => {
     try {
-      // Make a request to your authentication API
-      const response = await api.post("/users/login", { 
+      const response = await api.post("/users/login", {
         username,
         password,
       });
-      console.log(response);
-  
-      // Assuming your API returns a token upon successful login
-      const  token = response.data.token;
-      const  name  = response.data.name;
+
+      const token = response.data.token;
+      const name = response.data.name;
       const role = response.data.role;
-  
-      // Store the token in your authentication context or local storage
+
       Auth.authenticateUser(token);
-  
-      // Redirect the user to a protected route
-      navigate("/dashboard",{ state: { name,role } }); // Change "/dashboard" to your desired protected route
+
+      navigate("/dashboard", { state: { name, role } });
     } catch (error) {
-      // Handle login error
-      console.log(error)
       setError(`Invalid login ${error}`);
     }
   };
-  
 
   return (
-    <div
+    <Container
+      maxWidth="sm"
       style={{
         display: "flex",
         justifyContent: "center",
@@ -58,37 +66,72 @@ const LoginPage = () => {
         height: "100vh",
       }}
     >
-      <Card style={{ width: "300px" }}>
+      <Card>
         <CardContent>
-          <Typography variant="h5">Login</Typography>
-          {error && <div style={{ color: "red" }}>{error}</div>}
-          <TextField
-            label="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            fullWidth
-          />
-          <br />
-          <TextField
-            label="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            fullWidth
-          />
-          <br />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleLogin}
-            style={{ marginTop: "16px" }}
-            fullWidth
-          >
+          <Typography variant="h5" align="center">
             Login
-          </Button>
+          </Typography>
+          {error && (
+            <Typography variant="body2" color="error" align="center">
+              {error}
+            </Typography>
+          )}
+
+          <Box mt={2}>
+            <TextField
+              id="outlined-adornment-username"
+              label="Username"
+              variant="outlined"
+              fullWidth
+              onChange={(e) => setUsername(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <PersonIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+
+          <Box mt={2}>
+            <TextField
+              id="outlined-adornment-password"
+              label="Password"
+              variant="outlined"
+              fullWidth
+              type={showPassword ? "text" : "password"}
+              onChange={(e) => setPassword(e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+
+          <Box mt={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleLogin}
+              fullWidth
+            >
+              Login
+            </Button>
+          </Box>
         </CardContent>
       </Card>
-    </div>
+    </Container>
   );
 };
 
