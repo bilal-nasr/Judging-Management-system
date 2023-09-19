5c0fab842ba46fb25a66cf1b639c3582b34c4068
Diyaa5 <138508515+Diyaa5@users.noreply.github.com>
2023-09-19 17:35:31 +0300

    Update addJudge.js

--- frontend/src/Pages/Admin/addJudge.js
+++ frontend/src/Pages/Admin/addJudge.js
@@ -1,10 +1,16 @@
 import React from "react";
-
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material-next/Button';
 
 const addJudge = ()=>{
     return(
         <>
-            <h1>Here is form hello</h1>
+            
+            <TextField label="Username" color="secondary" focused />
+            <TextField label="Name" color="secondary" focused />
+            <TextField label="Role" color="secondary" focused />
+            <Button variant="Add">Text</Button>
+            <Button variant="Cancel">Text</Button>
         </>
     )
 }
