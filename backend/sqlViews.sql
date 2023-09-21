a89720cb5bc6478f9cfa42cd3bc0208e4882d837
bilal-nasr <bilal.nasr2711@gmail.com>
2023-09-21 22:03:08 +0300

    vb

--- backend/sqlViews.sql
+++ backend/sqlViews.sql
@@ -1,3 +1,7 @@
+
+--------------------------------
+-- Judges with users view
+-------------------------------
 CREATE VIEW JudgeView AS
 SELECT
     j.juryId AS JudgeId,
@@ -11,22 +15,29 @@ FROM
 JOIN
     users u
 ON
-    j.users_userId = u.userId;
+    j.users_userId = u.userId
+WHERE
+    u.role = 'J';
 
 
 
-CREATE VIEW StartupsWithBootcamps AS
+--------------------------------
+-- Bootcamp with startups view
+-------------------------------
+CREATE VIEW bootcamp_startups_view AS
 SELECT
-    s.startupId AS StartupId,
-    s.name AS StartupName,
-    s.description AS StartupDescription,
-    b.bootcampId AS BootcampId,
-    b.name AS BootcampName,
-    b.type AS BootcampType,
-    b.duration AS BootcampDuration
+    b.bootcampId,
+    b.name AS bootcamp_name,
+    b.type AS bootcamp_type,
+    b.year AS bootcamp_year,
+    s.startupId,
+    s.name AS startup_name,
+    s.description AS startup_description,
+    s.created_at AS startup_created_at,
+    s.updated_at AS startup_updated_at
 FROM
-    startups s
+    jmstest.bootcamps AS b
 LEFT JOIN
-    bootcamps b
+    jmstest.startups AS s
 ON
-    s.startupId = b.startups_startupId;
+    b.bootcampId = s.bootcamps_bootcampId;
