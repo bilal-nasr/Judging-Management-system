381e6189e6e69ec0a0d3ec76b04e86216f85267f
bilal-nasr <bilal.nasr2711@gmail.com>
2023-10-03 23:50:56 +0300

    update jury dasboard

--- frontend/src/Components/JuryDashboardComponent/Cards.js
+++ frontend/src/Components/JuryDashboardComponent/Cards.js
@@ -7,65 +7,65 @@ import Typography from '@mui/material/Typography';
 import studentIcon from '../../assets/JuryDashboard/StudentIcon.png'
 
 const cardStyle = {
-  maxWidth: 320,
-  margin: '20px auto',
-  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-  transition: 'transform 0.2s',
+    maxWidth: 320,
+    margin: '20px auto',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+    transition: 'transform 0.2s',
 };
 
 const cardHoverStyle = {
-  transform: 'scale(1.02)',
-  cursor: 'pointer'
+    transform: 'scale(1.1)',
+    cursor: 'pointer'
 };
 
 const avatarStyle = {
-  width: '100px',
-  height: '100px',
+    width: '100px',
+    height: '100px',
 };
 
 const contentStyle = {
-  textAlign: 'center',
+    textAlign: 'center',
 };
 
-const MyCard = () => {
-  const [isHovered, setIsHovered] = useState(false);
+const MyCard = (props) => {
+    const [isHovered, setIsHovered] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
 
-  const handleClick = ()=>{
-    console.log("hello")
-  }
+    const handleClick = (id) => {
+        console.log(id)
+    }
 
-  return (
-    <Card
-      style={{
-        ...cardStyle,
-        ...(isHovered && cardHoverStyle), // Apply hover styles when isHovered is true
-      }}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onClick={handleClick}
-    >
-      <CardHeader
-        avatar={
-          <Avatar alt="Remy Sharp" src={studentIcon} style={avatarStyle} />
-        }
-        title="Startup"
-        subheader="Startup"
-      />
-      <CardContent style={contentStyle}>
-        <Typography variant="body2" color="text.secondary">
-          More information about your startup goes here. Provide a brief description or details.
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card
+            style={{
+                ...cardStyle,
+                ...(isHovered && cardHoverStyle), // Apply hover styles when isHovered is true
+            }}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={() => handleClick(props.id)}
+        >
+            <CardHeader
+                avatar={
+                    <Avatar alt="Remy Sharp" src={studentIcon} style={avatarStyle} />
+                }
+                title="Startup"
+                subheader="Startup"
+            />
+            <CardContent style={contentStyle}>
+                <Typography variant="body2" color="text.secondary">
+                    More information about your startup goes here. Provide a brief description or details.
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 };
 
 export default MyCard;
--- frontend/src/Pages/Jury/JuryDashboard.js
+++ frontend/src/Pages/Jury/JuryDashboard.js
@@ -24,7 +24,7 @@ import Grid from '@mui/material/Unstable_Grid2';
 // }
 
 
-const AppBar = styled(MuiAppBar)();
+const AppBar = styled(MuiAppBar)(``);
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
@@ -37,15 +37,15 @@ export default function Dashboard() {
     const user = JSON.parse(localStorage?.getItem("user"))
     const name = user?.name || data?.userData.name;
 
-    const [dataGot,setDataGot] = React.useState(false)
+    // const [dataGot,setDataGot] = React.useState(false)
+    //TODO: get the startups of the bootcamp that the jury is evaluating
+    // const getStartups = async () => {
+    //     try {
 
-    const getStartups = async () => {
-        try {
-            
-        } catch (error) {
-            
-        }
-    }
+    //     } catch (error) {
+
+    //     }
+    // }
 
     return (
         <ThemeProvider theme={defaultTheme}>
@@ -85,8 +85,8 @@ export default function Dashboard() {
                         <Box sx={{ flexGrow: 1, marginLeft: "40px", marginTop: "50px" }}>
                             <Grid container spacing={2}>
                                 {Array.from(Array(20)).map((_, index) => (
-                                    <Grid item xs={12} sm={6} md={3} key={index}>
-                                            <Cards />
+                                    <Grid xs={12} sm={6} md={3} key={index}>
+                                        <Cards id={index} />
                                     </Grid>
                                 ))}
                             </Grid>
