1e4537923ce0c5bd05ab464b5e2b2fe0c4a76803
amjad-coder <138687423+amjad-coder@users.noreply.github.com>
2023-10-10 19:10:31 +0300

    Update StartupEvaluation.js

--- frontend/src/Pages/Jury/StartupEvaluation.js
+++ frontend/src/Pages/Jury/StartupEvaluation.js
@@ -1,36 +1,51 @@
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import api from "../../api";
+import { Button, Paper, Typography, Radio, RadioGroup, FormControl, FormControlLabel, Container } from "@mui/material";
 
+export default function StartupEvaluation() {
+  const [criterias, setCriterias] = useState([]);
+  const [dataGot, setDataGot] = useState(false);
 
-
-export default function StarupEvaluation() {
-
-    const [criterias, setCriterias] = useState([])
-    const [dataGot, setDataGot] = useState(false)
-
-    const getCriteria = async () => {
-        try {
-            const data = await api.get("/evaluation/getAllCriteria");
-            setDataGot(true)
-            setCriterias(data?.data.criteria)
-        } catch (error) {
-
-        }
+  const getCriteria = async () => {
+    try {
+      const data = await api.get("/evaluation/getAllCriteria");
+      setDataGot(true);
+      setCriterias(data?.data.criteria);
+    } catch (error) {
+      // Handle error
     }
+  };
 
-    useEffect(() => {
-        getCriteria()
-        console.log(criterias)
-        console.log(dataGot)
-    }, [dataGot])
+  useEffect(() => {
+    getCriteria();
+  }, []);
 
-    return (
-        <div>
-            {dataGot && criterias.map(criteria => {
-                return(
-                    <p>{criteria.description}</p>
-                )
-            })}
-        </div>
-    )
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h4" gutterBottom>
+        Startup Evaluation
+      </Typography>
+      {dataGot &&
+        criterias.map((criteria) => (
+          <Paper key={criteria.id} elevation={3} style={{ padding: "16px", marginBottom: "16px" }}>
+            <Typography variant="h6" style={{ fontWeight: "bold" }}>{criteria.description}</Typography>
+            <FormControl component="fieldset">
+              <RadioGroup row name={`rating_${criteria.id}`}>
+                {[1, 2, 3, 4, 5].map((rating) => (
+                  <FormControlLabel
+                    key={rating}
+                    value={rating.toString()}
+                    control={<Radio color="primary" />}
+                    label={rating.toString()}
+                  />
+                ))}
+              </RadioGroup>
+            </FormControl>
+          </Paper>
+        ))}
+      <Button variant="contained" color="primary">
+        Submit
+      </Button>
+    </Container>
+  );
 }
